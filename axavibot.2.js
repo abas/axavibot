8af0dd0486d0dfdea04f97548f890185133a4988
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-11 22:27:32 +0700

    test lib #2

---
+++ axavibot.2.js
@@ -0,0 +1,20 @@
+
+module['exports'] = function axaviBot(hook) {
+
+    // impor modul request
+    var request = require('request');
+    var R = require('./lib');
+
+    // reply msg
+    var msg = hook.params.message.text.toLowerCase();
+    var rep = R.Response(msg);
+
+    // buat atau kirim request ke bot telegram
+    request.post('https://api.telegram.org/bot' + hook.env.axavibot + '/sendMessage')
+        .form(
+            {
+            'chat_id': hook.params.message.chat.id,
+            'text': rep,
+
+        });
+}
