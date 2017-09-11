b9a547a00e21eb162ae66e8dcab78b848a61d557
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-12 01:46:01 +0700

    updated saru filter

--- lib.js
+++ lib.js
@@ -3,9 +3,10 @@ var axaviModule = {
     filterTextSaru : function (msg){
         // update jika ada masukan
         var textSaru = [
-            'cuk','cok','coeg','su','asu','anjing','njing','anying','nying',
-            'fuck','fak','fuk','kontol','kontil','goblok','bodo','bodoh',
-            'tai','tae','silit','bokong','ndas','ndes'
+            'cuk','cok','coeg','su','asu','anjing','njing','anying','nying','anjng',''
+            'fuck','fak','fuk','kontol','kontil','goblok','bodo','bodoh','tolol','kntl',
+            'tai','tae','silit','bokong','ndas','ndes','bento','koplo','suek','nds','fck',
+            
         ];
 
         var distance = textSaru.length;
