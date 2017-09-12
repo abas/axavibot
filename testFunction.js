0b1a3a4fd441dc6863a32651b5c31c5d8a3ac1cf
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-12 18:56:19 +0700

    test praising success

--- testFunction.js
+++ testFunction.js
@@ -3,3 +3,4 @@ var xavi = require('./lib')
 console.log(xavi.JerkFilter('gondes'))
 console.log(xavi.MathFilter('1+1'));
 console.log(xavi.NumFilter('jahvscjavja9'));
+console.log(xavi.PraiseFilter('mantaaaap'));
