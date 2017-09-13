e5f1d71294e9094f2cdac95fbbcfca67a563e9bf
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-13 08:05:19 +0700

    test lib success

--- testFunction.js
+++ testFunction.js
@@ -1,6 +1,8 @@
 var xavi = require('./lib')
 
 console.log(xavi.JerkFilter('gondes'))
-console.log(xavi.MathFilter('1+1'));
-console.log(xavi.NumFilter('jahvscjavja9'));
-console.log(xavi.PraiseFilter('mantaaaap'));
+console.log(xavi.MathFilter('1+1'))
+console.log(xavi.NumFilter('jahvscjavja9'))
+console.log(xavi.PraiseFilter('mantaaaap'))
+console.log(xavi.AbasVerify('Abas_'))
+console.log(xavi.AbasCommand('Abas_','xavii hari ini jadwal nya apa?'))
