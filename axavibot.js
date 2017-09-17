9b2ba25857ca902d5659cf143601b76dd2d00b39
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-17 21:16:18 +0700

    tes parse mode: #3

--- axavibot.js
+++ axavibot.js
@@ -387,12 +387,12 @@ module['exports'] = function axaviBot(hook) {
 
     // cek is thats me?
     if(getUsername()==='@ahmadbasir'){
-        request.post('https://api.telegram.org/bot' + hook.env.axavibot + '/sendMessage?parse_mode=true')
+        request.post('https://api.telegram.org/bot' + hook.env.axavibot + '/sendMessage?')
             .form(
                 {
                     'chat_id': hook.params.message.chat.id,
-                    'text': '__'+rep+'__',
-                    // 'parse_mode': rep,
+                    'text': rep,
+                    'parse_mode': '__'+rep+'__',
                 }
             );
         }
