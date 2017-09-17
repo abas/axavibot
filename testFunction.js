e90ebee48342946d072fc073671e724444d9e518
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-17 18:10:27 +0700

    tes knowOne success

--- testFunction.js
+++ testFunction.js
@@ -6,3 +6,4 @@ console.log(xavi.NumFilter('jahvscjavja9'))
 console.log(xavi.PraiseFilter('mantaaaap'))
 console.log(xavi.AbasVerify('Abas_'))
 console.log(xavi.AbasCommand('Abas_','xavii hari ini jadwal nya apa?'))
+console.log(xavi.KnowOne('kenal'));
