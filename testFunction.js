8c9d41d8895667a960ee83979aa19caf3310762a
ahmadbasir <akhmadbasir5@gmail.com>
2017-09-18 00:32:31 +0700

    tested funtion :success

--- testFunction.js
+++ testFunction.js
@@ -6,4 +6,7 @@ console.log(xavi.NumFilter('jahvscjavja9'))
 console.log(xavi.PraiseFilter('mantaaaap'))
 console.log(xavi.AbasVerify('Abas_'))
 console.log(xavi.AbasCommand('Abas_','xavii hari ini jadwal nya apa?'))
-console.log(xavi.KnowOne('kenal'));
+console.log(xavi.KnowOne('kenal'))
+console.log(xavi.IsMissed('kangen'))
+console.log(xavi.MissingOne('kamu kangen'))
+console.log(xavi.Emoji.haha)
