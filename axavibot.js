5d4e865c87b743c8562897fe457550834ea81476
abas <akhmadbasir5@gmail.com>
2018-03-18 19:41:45 +0700

    update response gender #11

--- axavibot.js
+++ axavibot.js
@@ -118,16 +118,6 @@ module['exports'] = function axaviBot(hook) {
             } else if (msg.includes('anu')) {
                 return 'anu apaan coba'
             } else if (msg.includes('gender')) {
-                request(simiUrl, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        var data = JSON.parse(body);
-                        request.post('https://api.telegram.org/bot' + hook.env.iyan_chan_bot + '/sendMessage')
-                        .form({
-                            "chat_id": hook.params.message.chat.id,
-                            "text": data.response
-                        });
-                    }
-                });
                 request('https://gender-api.com/get?name=elizabeth&key=' + genderapikey, (err, res, body) => {
                     if (!err && res.statusCode == 200) {
                         var data = JSON.parse(body);
